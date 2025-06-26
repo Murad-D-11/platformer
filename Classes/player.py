0cd1ff625b4dc7db622c351a1aec8085e5024643
Murad-D-11 <dashdamirov.murad11@gmail.com>
2025-06-26 14:08:20 -0400

    Update player.py
    - Add collisions

--- Classes/player.py
+++ Classes/player.py
@@ -6,6 +6,8 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = Spritesheet('sprite_sheet.png').parse_sprite('player_right.png')
         self.rect = self.image.get_rect()
+        self.rect.width -= 4
+        self.rect.height -= 4
         self.LEFT_KEY, self.RIGHT_KEY, self.FACING_LEFT = False, False, False
         self.is_jumping, self.on_ground = False, False
         self.gravity, self.friction =  0.35, -0.15
@@ -15,9 +17,12 @@ class Player(pygame.sprite.Sprite):
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
-    def update(self, dt):
+    def update(self, dt, tiles):
         self.horizontal_movement(dt)
+        self.check_collisions_x(tiles)
+
         self.vertical_movement(dt)
+        self.check_collisions_y(tiles)
 
     def horizontal_movement(self, dt):
         self.acceleration.x = 0
@@ -38,13 +43,6 @@ class Player(pygame.sprite.Sprite):
         self.velocity.y += self.acceleration.y * dt
         if self.velocity.y > 7: self.velocity.y = 7 # Limits vertical velocity
         self.position.y += self.velocity.y * dt + (self.acceleration.y * 0.5) * (dt * dt)
-
-        # Always lands on y = 128
-        if self.position.y > 128:
-            self.on_ground = True
-            self.velocity.y = 0
-            self.position.y = 128
-
         self.rect.bottom = self.position.y
 
     # Limits horizontal velocity
@@ -57,3 +55,40 @@ class Player(pygame.sprite.Sprite):
             self.is_jumping = True
             self.velocity.y -= 8 # Starting velocity
             self.on_ground = False
+
+    def get_hits(self, tiles):
+        hits = []
+        
+        for tile in tiles:
+            if self.rect.colliderect(tile):
+                hits.append(tile)
+
+        return hits
+    
+    def check_collisions_x(self, tiles):
+        collisions = self.get_hits(tiles)
+        
+        for tile in collisions:
+            if self.velocity.x > 0:
+                self.position.x = tile.rect.left - self.rect.w # X position is set to player's left side
+                self.rect.x = self.position.x
+            elif self.velocity.x < 0:
+                self.position.x = tile.rect.right
+                self.rect.x = self.position.x
+
+    def check_collisions_y(self, tiles):
+        self.on_ground = False
+        self.rect.bottom += 1 # Move hitbox by one pixel down
+        collisions = self.get_hits(tiles)
+
+        for tile in collisions:
+            if self.velocity.y > 0: # Hit tile from the top
+                self.on_ground = True
+                self.is_jumping = False
+                self.velocity.y = 0
+                self.position.y = tile.rect.top
+                self.rect.bottom = self.position.y
+            elif self.velocity.y < 0: # Hit tile from the bottom
+                self.velocity.y = 0
+                self.position.y = tile.rect.bottom + self.rect.h
+                self.rect.bottom = self.position.y
