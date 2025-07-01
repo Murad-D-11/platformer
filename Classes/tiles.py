3b56994e4fe13fed9d5782edf633a6cfe7774b96
Murad-D-11 <dashdamirov.murad11@gmail.com>
2025-07-01 18:02:12 -0400

    Update tiles.py

--- Classes/tiles.py
+++ Classes/tiles.py
@@ -9,10 +9,11 @@ class Tile(pygame.sprite.Sprite):
 
     def draw(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
+        pygame.draw.rect(surface, (255, 0, 0), self.rect, 1)
 
 class TileMap():
     def __init__(self, filename, spritesheet):
-        self.tile_size = 16
+        self.tile_size = 32
         self.start_x, self.start_y = 0, 0
         self.spritesheet = spritesheet
         self.tiles = self.load_tiles(filename)
@@ -26,8 +27,8 @@ class TileMap():
 
     # Loads map as one big surface
     def load_map(self):
-        for tile in self.tiles:
-            tile.draw(self.map_surface)
+        for entry in self.tiles:
+            entry.get('data').draw(self.map_surface)
 
     # Converts the .csv file into a list of rows
     def read_csv(self, filename):
@@ -49,14 +50,24 @@ class TileMap():
         for row in map:
             x = 0
             for tile in row:
-                if tile == '0': # Brick layer
-                    tiles.append(Tile('brick.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
-                elif tile == '1': # Surface
-                    tiles.append(Tile('pebbles.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
-                elif tile == '2': # Floating platform
-                    tiles.append(Tile('platform.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
-                elif tile == '3': # Player
+                if tile == '0': # Batoude
+                    tiles.append({'data': Tile('batoude.png', x * self.tile_size, y * self.tile_size, self.spritesheet), 'id': 0})
+                elif tile == '1': # Brick layer
+                    tiles.append({'data': Tile('brick.png', x * self.tile_size, y * self.tile_size, self.spritesheet), 'id': 1})
+                elif tile == '2': # Chain
+                    tiles.append({'data': Tile('chain.png', x * self.tile_size, y * self.tile_size, self.spritesheet), 'id': 2})
+                elif tile == '3': # Fin
+                    tiles.append({'data': Tile('fin.png', x * self.tile_size, y * self.tile_size, self.spritesheet), 'id': 3})
+                elif tile == '4': # Jump orb
+                    tiles.append({'data': Tile('jump_orb.png', x * self.tile_size, y * self.tile_size, self.spritesheet), 'id': 4})
+                elif tile == '6': # Surface
+                    tiles.append({'data': Tile('pebbles.png', x * self.tile_size, y * self.tile_size, self.spritesheet), 'id': 6})
+                elif tile == '7': # Floating platform
+                    tiles.append({'data': Tile('platform.png', x * self.tile_size, y * self.tile_size, self.spritesheet), 'id': 7})
+                elif tile == '9': # Player (facing right)
                     self.start_x, self.start_y = x * self.tile_size, y * self.tile_size
+                elif tile == '10': # Spike
+                    tiles.append({'data': Tile('spike.png', x * self.tile_size, y * self.tile_size, self.spritesheet), 'id': 10})
                 x += 1
             y += 1
 
