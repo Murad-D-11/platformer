097fe10f8492463c346b9a5547b24610809a035f
conciliatory <dashdamirov.murad11@gmail.com>
2025-08-14 12:44:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# TO-DO List:
-# Make the "Fin" tile operational
-# Add a bunch of other fun quirks
-# Create a menu and other GUI crap
-
 # External imports
 import pygame
 
@@ -14,27 +9,41 @@ from Classes.tiles import *
 
 pygame.init()
 
-# Other Objects Setup
 clock = pygame.time.Clock()
+
+icon = pygame.image.load('Images/icon.png')
+pygame.display.set_icon(icon)
+pygame.display.set_caption("Robo Trial: 8-Bit Chambers!")
 
 g = Game(None, None, None, clock)
 
 # Load player and spritesheet
 spritesheet = Spritesheet('sprite_sheet.png')
-player = Player()
+player = Player(g)
 
-# Loads and sets up the map (for debugging purposes)
-map = TileMap('Maps/level_3.csv', spritesheet)
-player.set_start_position(map.start_x, map.start_y)
+# Maps dictionary for quick access
+levels = {
+    1: TileMap('Maps/level_1.csv', spritesheet),
+    2: TileMap('Maps/level_2.csv', spritesheet),
+    3: TileMap('Maps/level_3.csv', spritesheet),
+    4: TileMap('Maps/level_4.csv', spritesheet),
+    5: TileMap('Maps/level_5.csv', spritesheet),
+}
 
 g.player = player
+g.levels = levels
 g.spritesheet = spritesheet
-g.map = map
 
 g.music_mixer()
 
 while g.running:
     g.current_menu.display_menu()
-    g.game_loop()
+
+    if g.playing:  # means a level was selected
+        selected_map = levels[g.current_level]
+        g.map = selected_map
+        g.player.set_start_position(selected_map.start_x, selected_map.start_y)
+
+        g.game_loop()
 
 pygame.quit()
